25375eb2eb8d5e6af9edd9d705705b926d215e87
Destroyer4Massive <pedro.m.onunes@gmail.com>
2021-04-06 20:02:37 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
 	public static void main(String[] args){
-		System.out.println("---*** Jenkins Training - changes webhook***---");
+		System.out.println("---*** Jenkins Training - changes webhook ***---");
 	}
 }
